c29921f26f4622b2e8bc3fe2a4568ae91afeb1a4
RimuEirnarn <Rimuru720@Proton.me>
2023-08-26 20:24:48 +0700

    terminal view

--- static/terminal.js
+++ static/terminal.js
@@ -1,1 +1,9 @@
 // Scripts are loaded once if they're a module, use event.
+
+let now = new Date()
+
+new Promise(resolve => setTimeout(resolve, 200)).then(() => {
+    $("#terminal-view").addClass("show")
+
+    $("#datetime").text(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`)
+})
