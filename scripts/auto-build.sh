f95eefbf53102c3dad6d8763174162c11e10ab43
RimuEirnarn <Rimuru720@Proton.me>
2023-09-18 20:52:40 +0700

    Configuring event listener on auto-build

--- scripts/auto-build.sh
+++ scripts/auto-build.sh
@@ -2,4 +2,4 @@
 
 . ./.venv/bin/activate
 
-watchmedo shell-command -R -w -W -p "*.js;*.css;*.html;*.mjs" -c 'scripts/wautobuild "${watch_event_type}" "${watch_object}"' public/ src/
+watchmedo shell-command -R -w -W -p "*.js;*.css;*.html;*.mjs" -c 'scripts/wautobuild "${watch_event_type}" "${watch_object}" "${watch_src_path}" "${watch_dest_path}"' public/ src/
